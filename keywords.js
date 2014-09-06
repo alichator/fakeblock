35af4c3e43ac3ec964782ac57a1ea132bfbeded3
sabburi <sabburi@umich.edu>
2014-09-06 12:10:31 -0400

    storage probs

--- keywords.js
+++ keywords.js
@@ -12,39 +12,50 @@
 	
 // };
 
+var keywordContainer = ["hello"];
 
 function addKeyword() {
 
-	var field = document.createElement("p");
-	field.innerHTML = document.getElementById("key").value;
-	
 	var filter = document.createElement("input");
 	filter.setAttribute("type", "checkbox");
+	filter.setAttribute("checked", "true");
+	filter.setAttribute("name", document.getElementById("key").value);
 	
-	var keywordField = document.createElement("div");
-	keywordField.appendChild(filter);
-	keywordField.appendChild(field);
-	document.body.appendChild(keywordField);
+	chrome.storage.local.get("keywords", function(keywords){
+		filter.setAttribute("id", "word" + keywords.length);
+	});
 
+	document.getElementById("main").appendChild(filter);
+
+	chrome.storage.local.get("keywords", function(keywords) {
+		keywords[keywords.length] = filter;
+	})
 }
 
 function loadKeywords() {
 
-	chrome.storage.sync.get(null, function (object items) {
-		
-	})
+	chrome.storage.local.get("keywords", function (keywords) {
+		for(var i = 0; i < keywords.length; ++i)
+		{
+			document.getElementById("main").appendChild(keywords[i]);
+		}
+	});
 
 }
 
 document.addEventListener("DOMContentLoaded", function(){
 	
-	loadKeywords();
+	chrome.storage.local.set({"keywords": "hello"});
+	//loadKeywords();
 
-
-
-
-	document.getElementById("inputbox").addEventListener("submit", function(){
-		addKeyword();
+	chrome.storage.local.get("keywords", function(a){
+		window.alert("keywords" + a);
 	});
+
+
+	// document.getElementById("inputbox").addEventListener("submit", function(){
+	// 	addKeyword();
+	// 	loadKeywords();
+	// });
 
 });

e61fa9c835c48cbb4b242d5515e5bf4d5e3edbe3
sabburi <sabburi@umich.edu>
2014-09-06 10:38:45 -0400

    figured out how to add elements. ye.

--- keywords.js
+++ keywords.js
@@ -1,23 +1,30 @@
-function addCheckbox() {
-		/*
-		var text = document.getElementById("keyword").value;
-		var filter = document.createElement("INPUT");
-		filter.setAttribute("type", "checkbox");
-		//filter.setAttribute("checked", "true");
-		document.body.appendChild(filter);
-		*/
-		var text = document.getElementById("key").value;
-		console.log(text);
-		document.body.appendChild(document.createElement("BUTTON"));
+// function addCheckbox() {
+// 		/*
+// 		var text = document.getElementById("keyword").value;
+// 		var filter = document.createElement("INPUT");
+// 		filter.setAttribute("type", "checkbox");
+// 		//filter.setAttribute("checked", "true");
+// 		document.body.appendChild(filter);
+// 		*/
+// 		var text = document.getElementById("key").value;
+// 		console.log(text);
+// 		document.body.appendChild(document.createElement("BUTTON"));
 	
-}
+// };
 
 document.addEventListener("DOMContentLoaded", function(){
-	document.getElementById("inputbox").addEventListener("onsubmit", function(){
-		var text = document.getElementById("key").value;
-		console.log(text);
-		document.body.appendChild(document.createElement("BUTTON"));
+	document.getElementById("inputbox").addEventListener("submit", function(){
+		var field = document.createElement("p");
+		field.innerHTML = document.getElementById("key").value;
+		
+		var filter = document.createElement("input");
+		filter.setAttribute("type", "checkbox");
+		
+
+		var keywordField = document.createElement("div");
+		keywordField.appendChild(filter);
+		keywordField.appendChild(field);
+		document.body.appendChild(keywordField);
 	});
 
 });
-	

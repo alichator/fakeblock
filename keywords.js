8207995cbd860f25f46f3db37bbecd151a76d684
sabburi <sabburi@umich.edu>
2014-09-06 11:09:22 -0400

    added functions

--- keywords.js
+++ keywords.js
@@ -12,19 +12,39 @@
 	
 // };
 
-document.addEventListener("DOMContentLoaded", function(){
-	document.getElementById("inputbox").addEventListener("submit", function(){
-		var field = document.createElement("p");
-		field.innerHTML = document.getElementById("key").value;
-		
-		var filter = document.createElement("input");
-		filter.setAttribute("type", "checkbox");
-		
 
-		var keywordField = document.createElement("div");
-		keywordField.appendChild(filter);
-		keywordField.appendChild(field);
-		document.body.appendChild(keywordField);
+function addKeyword() {
+
+	var field = document.createElement("p");
+	field.innerHTML = document.getElementById("key").value;
+	
+	var filter = document.createElement("input");
+	filter.setAttribute("type", "checkbox");
+	
+	var keywordField = document.createElement("div");
+	keywordField.appendChild(filter);
+	keywordField.appendChild(field);
+	document.body.appendChild(keywordField);
+
+}
+
+function loadKeywords() {
+
+	chrome.storage.sync.get(null, function (object items) {
+		
+	})
+
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+	
+	loadKeywords();
+
+
+
+
+	document.getElementById("inputbox").addEventListener("submit", function(){
+		addKeyword();
 	});
 
 });

26b398d57e9f5a69706fb2d28f3d9f7fd1f997d0
sabburi <sabburi@umich.edu>
2014-09-07 09:30:37 -0400

    got keywords

--- eventpage.js
+++ eventpage.js
@@ -1,2 +1,12 @@
+var container = [];
 chrome.webNavigation.onDOMContentLoaded.addListener(function(e) {
+   chrome.storage.sync.get("keys", function(a){
+	for(var i=0; i<a.keys.length; i++){
+	    if(a.keys[i].check == "t"){
+		container.push(a.keys[i].key);
+	     }
+	}
+   });
+   console.log(container)
+
 }, {url: [{hostSuffix: 'twitter.com'}]});

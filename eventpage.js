1d9ebbd484c41bf9c5031166462ea33c7c96aefb
Alishah Chator <alishahc@umich.edu>
2014-09-07 00:44:46 -0400

    changed target site to twitter

--- eventpage.js
+++ eventpage.js
@@ -1,5 +1,2 @@
 chrome.webNavigation.onDOMContentLoaded.addListener(function(e) {
-        num = 1;
-        console.log(num);
-        num += 1
-}, {url: [{hostSuffix: 'facebook.com'}]});
+}, {url: [{hostSuffix: 'twitter.com'}]});
